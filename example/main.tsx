af6f1c97f36b4b3890e4d1120e01111168ca5131
Yash Kanekar <kanekaryash.11@gmail.com>
2025-05-21 00:50:40 +0530

    refactor: remove unused Tooltip and Overlay components; update type exports in index.ts

--- example/main.tsx
+++ example/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "../src/styles/walkthrough.css"; // Import styles from the library
+import "../src/styles/walkthrough.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
